f3a2e7f422f20d016a9097365ce105143e758384
aarya22 <aman.arya524@gmail.com>
2017-06-16 18:12:35 -0400

    Made minor changes and switch trk save with picture making

--- tracking_AA.py
+++ tracking_AA.py
@@ -76,11 +76,11 @@ def prob_model(data_file, data_bvec, data_bval, data_fs_seg, data_brainmask):
                                  relative_peak_threshold=.8,
                                  min_separation_angle=45,
                                  mask=wm_mask)
-    print('Creating CSA Model ' + str(time.time() - start))
+    print('Creating CSA Model: ' + str(time.time() - start))
 
     # Begins the seed in the wm tracts
     seeds = utils.seeds_from_mask(wm_mask, density=[1, 1, 1], affine=affine)
-    print('Created White Matter seeds ' + str(time.time() - start))
+    print('Created White Matter seeds: ' + str(time.time() - start))
 
     # Create a CSD model to measure Fiber Orientation Dist
     print('Begin the probabilistic model')
@@ -88,28 +88,33 @@ def prob_model(data_file, data_bvec, data_bval, data_fs_seg, data_brainmask):
     response, ratio = auto_response(gtab, dmri, roi_radius=10, fa_thr=0.7)
     csd_model = ConstrainedSphericalDeconvModel(gtab, response, sh_order=6)
     csd_fit = csd_model.fit(dmri, mask=wm_mask)
-    print ('Created the CSD model ' + str(time.time() - start))
+    print ('Created the CSD model: ' + str(time.time() - start))
 
     # Set the Direction Getter to randomly choose directions
 
     prob_dg = ProbabilisticDirectionGetter.from_shcoeff(csd_fit.shm_coeff,
                                                         max_angle=30.,
                                                         sphere=default_sphere)
-    print('Created the Direction Getter ' + str(time.time() - start))
+    print('Created the Direction Getter: ' + str(time.time() - start))
 
     # Restrict the white matter tracking
     classifier = ThresholdTissueClassifier(csa_peaks.gfa, .25)
 
-    print('Created the Tissue Classifier ' + str(time.time() - start))
+    print('Created the Tissue Classifier: ' + str(time.time() - start))
 
     # Create the probabilistic model
     streamlines = LocalTracking(prob_dg, tissue_classifier=classifier, seeds=seeds, affine=affine,
                                 step_size=.5, max_cross=1)
-    print('Created the probabilistic model ' + str(time.time() - start))
+    print('Created the probabilistic model: ' + str(time.time() - start))
 
     # Compute streamlines and store as a list.
     streamlines = list(streamlines)
-    print('Computed streamlines ' + str(time.time() - start))
+    print('Computed streamlines: ' + str(time.time() - start))
+
+    tractogram = Tractogram(streamlines, affine_to_rasmm=affine)
+    save(tractogram, 'csa_prob.trk')
+    end = time.time()
+    print("Created the trk file: " + str((end - start)))
 
     # Prepare the display objects.
     print("Making pretty pictures")
@@ -121,12 +126,10 @@ def prob_model(data_file, data_bvec, data_bval, data_fs_seg, data_brainmask):
         # Save still images for this static example.
         fvtk.record(r, n_frames=1, out_path='probabilistic.png',
                     size=(800, 800))
+
     print ('Made pretty pictures')
 
 
-    tractogram = Tractogram(streamlines, affine_to_rasmm=affine)
-    save(tractogram, 'csa_prob.trk')
-    end = time.time()
-    print("Created the trk file: " + str((end - start)))
+
 
 prob_model(data_file, data_bvec, data_bval, data_fs_seg, data_brainmask)
